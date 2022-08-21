29feee332aa4b96ac5c0bc66a773722a7338b82d
Evandro Silva <evandro.01silva@gmail.com>
2022-08-21 20:18:36 -0300

    pulo do mario completado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mario = window.document.querySelector('.mario');
+const pipe = window.document.querySelector('.pipe');
 
 const jump = ()=>{
     mario.classList.add('jump');
@@ -7,5 +8,17 @@ const jump = ()=>{
         mario.classList.remove('jump');
     }, 500);
 }
+
+const loop = setInterval(() => {
+
+    const pipeposition = pipe.offsetLeft;
+    const marioposition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if(pipeposition <= 110 && pipeposition > 5 && marioposition < 70){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipeposition}px`;
+    }
+
+}, 10)
 
 window.document.addEventListener('keydown', jump);

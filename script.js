2ebcb20e3ca87058ac8ff328341c4eeb1a55e465
Evandro Silva <evandro.01silva@gmail.com>
2022-08-25 21:53:31 -0300

    Aula finalizada

--- script.js
+++ script.js
@@ -17,6 +17,14 @@ const loop = setInterval(() => {
     if(pipeposition <= 110 && pipeposition > 5 && marioposition < 70){
         pipe.style.animation = 'none';
         pipe.style.left = `${pipeposition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioposition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+
+        clearInterval(loop);
     }
 
 }, 10)
